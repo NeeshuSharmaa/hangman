fe1adb10767f57ca140663072e814d3db783d416
Neeshu Sharma <114111237+NeeshuSharmaa@users.noreply.github.com>
2023-10-27 18:05:35 +0530

    style: HangmanWord

--- src/components/HangmanWord.tsx
+++ src/components/HangmanWord.tsx
@@ -1,3 +1,30 @@
 export default function HangmanWord() {
-  return <div>HangmanWord</div>;
+  const word = "mark";
+  const guessedLetters = ["m", "e", "g"];
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        fontSize: "6rem",
+        gap: "0.25em",
+        textTransform: "uppercase",
+        fontFamily: "monospace",
+      }}
+    >
+      {word.split("").map((letter, index) => (
+        <span key={index} style={{ borderBottom: "10px solid black" }}>
+          <span
+            style={{
+              visibility: guessedLetters.includes(letter)
+                ? "visible"
+                : "hidden",
+            }}
+          >
+            {letter}
+          </span>
+        </span>
+      ))}
+    </div>
+  );
 }
