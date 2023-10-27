ceaa345458cc2fb5b92bbd2510f42e60968c755c
Neeshu Sharma <114111237+NeeshuSharmaa@users.noreply.github.com>
2023-10-28 00:26:22 +0530

    feat: reveal words if lose in red

--- src/components/HangmanWord.tsx
+++ src/components/HangmanWord.tsx
@@ -1,11 +1,15 @@
 type HangmanWordProps = {
+  isLoser: boolean;
   wordToGuess: string;
   guessedLetters: string[];
 };
 export default function HangmanWord({
+  isLoser,
   wordToGuess,
   guessedLetters,
 }: HangmanWordProps) {
+  const reveal = isLoser;
+  console.log(reveal);
   return (
     <div
       style={{
@@ -14,15 +18,19 @@ export default function HangmanWord({
         gap: "0.25em",
         textTransform: "uppercase",
         fontFamily: "monospace",
+        alignSelf: "center",
       }}
     >
       {wordToGuess.split("").map((letter, index) => (
         <span key={index} style={{ borderBottom: "6px solid rgb(26, 32, 44)" }}>
           <span
             style={{
-              visibility: guessedLetters.includes(letter)
-                ? "visible"
-                : "hidden",
+              visibility:
+                guessedLetters.includes(letter) || reveal
+                  ? "visible"
+                  : "hidden",
+              color:
+                !guessedLetters.includes(letter) && reveal ? "red" : "black",
             }}
           >
             {letter}
